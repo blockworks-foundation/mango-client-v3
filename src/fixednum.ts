ec706778faf13c38329809da71708d0e7a30391c
dd <dafydd.durairaj@gmail.com>
2021-05-22 13:08:00 -0400

    Added skeleton code for fixednum library; Added Keeper file; no code there

---
+++ src/fixednum.ts
@@ -0,0 +1,53 @@
+import BN from 'bn.js';
+
+
+export class I80F48 {
+  /*
+  This is represented by a 128 bit signed integer underneath
+  The first 80 bits are treated as an integer and last 48 bits are treated as fractional part after binary point
+  It's possible to think of an I80F48 as an i128 divided by 2 ^ 40
+
+  Read up on how fixed point math works: https://inst.eecs.berkeley.edu/~cs61c/sp06/handout/fixedpt.html
+  Read up on how 2s complement works: https://en.wikipedia.org/wiki/Two%27s_complement
+   */
+
+  data: BN;  // This is i128 => array of 16 bytes
+
+  constructor(data: BN) {
+    this.data = data;
+  }
+
+  fromFloat(x: number): I80F48 {
+    throw new Error("Not Implemented")
+  }
+
+  toFloat(): number {
+    throw new Error("Not Implemented")
+  }
+
+  toString(): string {
+    throw new Error("Not Implemented")
+  }
+
+
+  toArray(): Uint8Array {
+    throw new Error("Not Implemented")
+  }
+
+  add(x: I80F48): I80F48 {
+    throw new Error("Not Implemented")
+  }
+
+  sub(x: I80F48): I80F48 {
+    throw new Error("Not Implemented")
+  }
+
+  mul(x: I80F48): I80F48 {
+    throw new Error("Not Implemented")
+  }
+
+  div(x: I80F48): I80F48 {
+    throw new Error("Not Implemented")
+  }
+
+}
---
+++ src/keeper.ts
@@ -0,0 +1,11 @@
+
+/*
+This will probably move to its own repo at some point but easier to keep it here for now
+This will be a long running program that will call all the Keeper related instructions on-chain
+
+This will be very similar to the crank in serum dex.
+ */
+
+export class Keeper {
+
+}
