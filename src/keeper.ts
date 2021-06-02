f8bdc740b57c7543a7c3fd6466d617f0e9b13fcc
Riordan Panayides <riordan@panayid.es>
2021-06-02 18:54:16 +0100

    Start keeper

--- src/keeper.ts
+++ src/keeper.ts
@@ -5,20 +5,46 @@ This will be a long running program that will call all the Keeper related instru
 This will be very similar to the crank in serum dex.
 
  */
+import * as os from 'os';
+import * as fs from 'fs';
+import { MerpsClient } from './client';
+import { Account, Commitment, Connection, PublicKey } from '@solana/web3.js';
 import { sleep } from './utils';
 
 export class Keeper {
   /**
    * Long running program that never exits except on keyboard interrupt
    */
-  run() {
+  async run() {
     const interval = 5000;
     while (true) {
-      sleep(interval);
+      await sleep(interval);
+      // TODO: Fetch ids from ids.json
+      // TODO: Get cluster and keypair from env
+      const merpsProgramId = new PublicKey(
+        'EBXaJhhjhRKYDRNwHUgqJhMDWGNqKwpwD3sYkXRN9Yuz',
+      );
+      const dexProgramId = new PublicKey(
+        'DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY',
+      );
+      const merpsGroupKey = new PublicKey('');
+      const payer = new Account(
+        JSON.parse(
+          fs.readFileSync(
+            os.homedir() + '/.config/solana/devnet.json',
+            'utf-8',
+          ),
+        ),
+      );
+      const connection = new Connection(
+        'https://devnet.solana.com',
+        'processed' as Commitment,
+      );
+      const client = new MerpsClient(connection, merpsProgramId);
+      const merpsGroup = await client.getMerpsGroup(merpsGroupKey);
 
-      /*
-        First task: update the MerpsCache with the RootBank
-       */
+      // update the MerpsCache with the RootBank
+      await client.cacheRootBanks(payer, merpsGroupKey, merpsGroup.merpsCache);
     }
   }
 }
