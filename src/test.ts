643959eef06c9afbaa048f9c94e58b4fbdb3bb36
Tyler Shipe <tjshipe@gmail.com>
2021-06-11 16:28:13 -0400

    cleanup test.ts

--- src/test.ts
+++ src/test.ts
@@ -11,68 +11,44 @@ function assertEq(msg, a, b) {
   }
 }
 
-// export const BTCMint = new PublicKey(
-//   '3ZTThPgKSNbMzT3yK9Y43fU2ZMuQ6jprwpgfpvDw7b8j',
-// );
-// export const BTCFaucet = new PublicKey(
-//   'FnS8ceYHXUarddKcFCGrFMVFWqfRku2bEippH6FtkDX6',
-// );
-// export const FAUCET_PROGRAM_ID = new PublicKey(
-//   '4bXpkKSV8swHSnwqtzuboGPaPDeEgAn4Vt8GfarV5rZt',
-// );
+const merpsProgramId = new PublicKey(
+  'H3Xu3m3qiYZmXcdUDTEhQNJSui7bcfziphw8LGvot9Hp',
+);
+const dexProgramId = new PublicKey(
+  'DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY',
+);
+const payerQuoteTokenAcc = new PublicKey(
+  'GZira1ybvTWJsZEvqgjSuohir2uAvd1gV9joVvS6K9xr',
+);
+const quoteMintKey = new PublicKey(
+  'H6hy7Ykzc43EuGivv7VVuUKNpKgUoFAfUY3wdPr4UyRX',
+);
+const connection = new Connection(
+  'https://api.devnet.solana.com',
+  'processed' as Commitment,
+);
+const payer = new Account(
+  JSON.parse(
+    fs.readFileSync(
+      os.homedir() + '/my-solana-wallet/my-keypair.json',
+      'utf-8',
+    ),
+  ),
+);
 
 async function test() {
-  console.log('starting test');
-
-  const merpsProgramId = new PublicKey(
-    'H3Xu3m3qiYZmXcdUDTEhQNJSui7bcfziphw8LGvot9Hp',
-  );
-  const dexProgramId = new PublicKey(
-    'DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY',
-  );
-  const payer = new Account(
-    JSON.parse(
-      fs.readFileSync(
-        os.homedir() + '/my-solana-wallet/my-keypair.json',
-        'utf-8',
-      ),
-    ),
-  );
-  const payerQuoteTokenAcc = new PublicKey(
-    'GZira1ybvTWJsZEvqgjSuohir2uAvd1gV9joVvS6K9xr',
-  );
-
-  // const userBtcPk = new PublicKey('userBtcPk');
-  console.log('creating connection');
-
-  const connection = new Connection(
-    'https://api.devnet.solana.com',
-    'processed' as Commitment,
-  );
-  console.log('connection created');
-
   const client = new MerpsClient(connection, merpsProgramId);
-
-  const quoteMintKey = new PublicKey(
-    'H6hy7Ykzc43EuGivv7VVuUKNpKgUoFAfUY3wdPr4UyRX',
-  );
-  console.log('creating merps group');
-
   const groupKey = await client.initMerpsGroup(
     payer,
     quoteMintKey,
     dexProgramId,
     5,
   );
-  console.log('merps group created', groupKey);
-
-  await sleep(3000);
   const merpsGroup = await client.getMerpsGroup(groupKey);
   await sleep(3000);
   const rootBanks = await merpsGroup.loadRootBanks(client.connection);
   const usdcRootBank = rootBanks[QUOTE_INDEX];
-
-  console.log('Group Created:', merpsGroup.publicKey.toBase58());
+  console.log('merpsGroup Created:', merpsGroup.publicKey.toBase58());
 
   assertEq(
     'quoteMint',
@@ -95,7 +71,6 @@ async function test() {
   );
 
   if (!usdcRootBank) throw new Error('no root bank');
-  console.log('get node banks');
 
   const nodeBanks = await usdcRootBank.loadNodeBanks(client.connection);
   const filteredNodeBanks = nodeBanks.filter((nodeBank) => !!nodeBank);
@@ -118,7 +93,6 @@ async function test() {
     );
   } catch (err) {
     console.log('Error on deposit', `${err}`);
-    console.log('txid', err?.txid);
   }
 
   // const cacheRootBanksTxID = await client.cacheRootBanks(
