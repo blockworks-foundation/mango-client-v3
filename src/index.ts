4f17ed73c9ed5ba8e9b0798f54d39e814dba42d7
Tyler Shipe <tjshipe@gmail.com>
2021-06-16 17:15:29 -0400

    add more exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import IDS from './ids.json';
+import MerpsGroup from './MerpsGroup';
+import MerpsAccount from './MerpsAccount';
 
 export { MerpsClient } from './client';
-export { IDS };
+export { MerpsAccountLayout, MerpsGroupLayout } from './layout';
+
+export * from './layout';
+export * from './utils';
+export { IDS, MerpsGroup, MerpsAccount };
