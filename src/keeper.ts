fb47a9535a9eefaba114a6aa16854024654e8958
Maximilian Schneider <mail@maximilianschneider.net>
2021-06-18 16:06:24 -0400

    upgrade keeper to v2

--- src/keeper.ts
+++ src/keeper.ts
@@ -36,7 +36,7 @@ export class Keeper {
     const config = new Config(configFile);
 
     const cluster = (process.env.CLUSTER || 'devnet') as Cluster;
-    const groupName = process.env.GROUP || 'merps_test_v1';
+    const groupName = process.env.GROUP || 'merps_test_v2';
     const groupIds = config.getGroup(cluster, groupName);
 
     if (!groupIds) {
