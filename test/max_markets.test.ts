bd159f89da81bbad5e4fc2724cdf1816785e73a1
Ralfs <lagzda7@inbox.lv>
2021-07-14 01:18:33 +0300

    Add tokens to mango group implemented with errors

--- test/max_markets.test.ts
+++ test/max_markets.test.ts
@@ -8,6 +8,10 @@ import MangoGroup, { QUOTE_INDEX } from '../src/MangoGroup';
 import { sleep, zeroKey } from '../src/utils';
 import MangoAccount from '../src/MangoAccount';
 
+const MAX_RATE = 1.5;
+const OPTIMAL_UTIL = 0.7;
+const OPTIMAL_RATE = 0.06;
+
 describe('MaxMarkets', async () => {
   let client: MangoClient;
   let payer: Account;
@@ -31,14 +35,47 @@ describe('MaxMarkets', async () => {
         1.5,
         payer,
       );
+      const mangoGroup = await client.getMangoGroup(mangoGroupPk);
       console.log("mangoGroupPk:", mangoGroupPk.toString());
+      const num = 2;
       // Create mints
-      const mints: Token[] = await Test.createMints(connection, payer, 2);
+      const mints: Token[] = await Test.createMints(connection, payer, num);
       // List spot markets
       const spotMarketPks = await Test.listMarkets(connection, payer, Test.DexProgramId, mints, Test.USDCMint);
       // Add markets to MangoGroup
-      for (let spotMarketPk of spotMarketPks) {
+      for (let i = 0; i < num; i++) {
+        const mint = mints[i];
+        const spotMarketPk = spotMarketPks[i];
+
+        const oraclePk = await Test.createOracle(connection, Test.MangoProgramId, payer);
+        try {
+          await client.addOracle(mangoGroup, oraclePk, payer);
+        } catch (err) {
+          console.log(err);
+          // TODO: In mango-v3 program code
+          // the add_oracle expects oracle_ai to writeable but it's not
+          // We need it to be writeable to add our magic bytes
+          // If that will mess up Pyth oracle addition
+          // We need 2 add_oracle fns in processor.rs - add_stub_oracle and add_oracle
+        }
+
+        // TODO: Set oracle to whatever is necessary
+
+        const initLeverage = 5;
+        const maintLeverage = initLeverage * 2;
         console.log("spotMarketPk:", spotMarketPk.toString());
+        await client.addSpotMarket(
+          mangoGroup,
+          spotMarketPk,
+          mint.publicKey,
+          payer,
+          i,
+          maintLeverage,
+          initLeverage,
+          OPTIMAL_UTIL,
+          OPTIMAL_RATE,
+          MAX_RATE,
+        );
       }
     });
   });
