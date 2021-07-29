1e901ad7eb6e29095e5b7f1605ed1f3536de4e74
Tyler Shipe <tjshipe@gmail.com>
2021-07-29 11:26:47 -0400

    fix account naming instruction

--- src/client.ts
+++ src/client.ts
@@ -2022,8 +2022,8 @@ export class MangoClient {
     const instruction = makeAddMangoAccountInfoInstruction(
       this.programId,
       mangoGroup.publicKey,
-      owner.publicKey,
       mangoAccount.publicKey,
+      owner.publicKey,
       info,
     );
     const transaction = new Transaction();
