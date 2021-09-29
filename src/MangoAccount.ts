75bb76675224658519b98951d4821966c41c8614
Tyler Shipe <tjshipe@gmail.com>
2021-09-29 12:19:10 -0400

    use getPrice in getLiabsVal

--- src/MangoAccount.ts
+++ src/MangoAccount.ts
@@ -242,7 +242,7 @@ export default class MangoAccount {
 
     for (let i = 0; i < mangoGroup.numOracles; i++) {
       let liabWeight = ONE_I80F48;
-      const price = mangoCache.priceCache[i].price;
+      const price = mangoGroup.getPrice(i, mangoCache);
       if (healthType === 'Maint') {
         liabWeight = mangoGroup.spotMarkets[i].maintLiabWeight;
       } else if (healthType === 'Init') {
