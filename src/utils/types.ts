0cab567637a5e14169625ae4ac73a22e770e0715
Kieran Gillen <kgillen@gmail.com>
2022-03-15 15:55:26 +0100

    Updates WalletAdapter type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,13 +4,11 @@ import { PublicKey, Transaction } from '@solana/web3.js';
 export type Modify<T, R> = Omit<T, keyof R> & R;
 export interface WalletAdapter {
   publicKey: PublicKey;
-  autoApprove: boolean;
   connected: boolean;
   signTransaction: (transaction: Transaction) => Promise<Transaction>;
   signAllTransactions: (transaction: Transaction[]) => Promise<Transaction[]>;
   connect: () => any;
   disconnect: () => any;
-  on(event: string, fn: () => void): this;
 }
 
 export type PerpOrderType =
