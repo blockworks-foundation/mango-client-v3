e4d032607967670bafb058fa538ef055550ee9b6
Kieran Gillen <kgillen@gmail.com>
2022-03-31 12:37:59 +0200

    Adds nullable checks for adapter

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -182,6 +182,9 @@ yargs(hideBin(process.argv)).command(
       args.quote_optimal_rate as number,
       args.quote_max_rate as number,
     );
+    if (!result) {
+      return;
+    }
     console.log(result);
     config.storeGroup(result);
     writeConfig(args.config as string, config);
