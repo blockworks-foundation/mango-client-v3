6d03133f19c4b42adc5001f6a203b85ab2f4d0b5
Maximilian Schneider <mail@maximilianschneider.net>
2022-11-13 00:59:21 +0100

    relaunch serum

---
+++ src/scripts/lots.ts
@@ -0,0 +1,79 @@
+import { Market } from '@project-serum/serum';
+import { Commitment, Connection, PublicKey } from '@solana/web3.js';
+import { MangoClient } from '../client';
+import { Cluster, Config } from '../config';
+
+async function main() {
+  const config = Config.ids();
+  const cluster = (process.env.CLUSTER || 'mainnet') as Cluster;
+  const connection = new Connection(
+    config.cluster_urls[cluster],
+    'processed' as Commitment,
+  );
+
+  const groupName = process.env.GROUP || 'mainnet.1';
+  const groupIds = config.getGroup(cluster, groupName);
+  if (!groupIds) {
+    throw new Error(`Group ${groupName} not found`);
+  }
+
+  const mangoProgramId = groupIds.mangoProgramId;
+  const mangoGroupKey = groupIds.publicKey;
+  const client = new MangoClient(connection, mangoProgramId);
+
+  for (let pk of [
+    '9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT',
+    '77quYg4MGneUdjgXCunt9GgM1usmrxKY31twEy3WHwcS',
+    '6oGsL2puUgySccKzn9XA9afqF217LfxP5ocq4B3LWsjy',
+    '8Gmi2HhZmwQPVdCwzS7CM66MGstMXPcTVHA7jF19cLZz',
+  ]) {
+    let mkt = await Market.load(
+      connection,
+      new PublicKey(pk),
+      undefined,
+      groupIds.serumProgramId,
+    );
+
+    console.log('pk:', pk);
+    const {
+      eventQueue,
+      requestQueue,
+      bids,
+      asks,
+      quoteDustThreshold,
+      feeRateBps,
+      baseLotSize,
+      quoteLotSize,
+    } = mkt['_decoded'];
+
+    console.log('minOrderSize', mkt.minOrderSize);
+    console.log('tickSize', mkt.tickSize);
+
+    console.log('baseLotSize', baseLotSize.toNumber());
+    console.log('quoteLotSize', quoteLotSize.toNumber());
+    console.log('feeRateBps', feeRateBps.toNumber());
+    console.log('quoteDustThreshold', quoteDustThreshold.toNumber());
+
+    const [eq, rq, bs, as] = await Promise.all([
+      connection.getAccountInfo(eventQueue),
+      connection.getAccountInfo(requestQueue),
+      connection.getAccountInfo(bids),
+      connection.getAccountInfo(asks),
+    ]);
+
+    console.log('eventQueueSize', eq?.data.byteLength);
+    console.log('requestQueueSize', rq?.data.byteLength);
+    console.log('bidsSize', bs?.data.byteLength);
+    console.log('asksSize', as?.data.byteLength);
+
+    console.log(
+      'sol',
+      (eq!.lamports + bs!.lamports + as!.lamports) / 1_000_000_000,
+    );
+
+    console.log('------------------------');
+    // console.log(mkt['_decoded']);
+  }
+}
+
+main();
---
+++ src/scripts/serumCrank.ts
@@ -0,0 +1,152 @@
+/**
+ This will probably move to its own repo at some point but easier to keep it here for now
+ */
+import * as os from 'os';
+import * as fs from 'fs';
+import markets from '../serum.json';
+import {
+  Keypair,
+  Commitment,
+  Connection,
+  PublicKey,
+  Transaction,
+} from '@solana/web3.js';
+import { getMultipleAccounts, sleep } from '../utils/utils';
+import BN from 'bn.js';
+import {
+  decodeEventQueue,
+  DexInstructions,
+  Market,
+} from '@project-serum/serum';
+import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+
+const {
+  ENDPOINT_URL,
+  KEYPAIR,
+  PROGRAM_ID,
+  INTERVAL,
+  MAX_UNIQUE_ACCOUNTS,
+  CONSUME_EVENTS_LIMIT,
+} = process.env;
+
+const interval = INTERVAL || 3500;
+const maxUniqueAccounts = parseInt(MAX_UNIQUE_ACCOUNTS || '10');
+const consumeEventsLimit = new BN(CONSUME_EVENTS_LIMIT || '10');
+const serumProgramId = new PublicKey(
+  PROGRAM_ID || 'srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX',
+);
+
+const payer = Keypair.fromSecretKey(
+  Uint8Array.from(
+    JSON.parse(
+      KEYPAIR ||
+        fs.readFileSync(os.homedir() + '/.config/solana/devnet.json', 'utf-8'),
+    ),
+  ),
+);
+
+const connection = new Connection(ENDPOINT_URL!, 'processed' as Commitment);
+
+async function run() {
+  const spotMarkets = await Promise.all(
+    markets.map((m) => {
+      return Market.load(
+        connection,
+        new PublicKey(m.publicKey),
+        {
+          skipPreflight: true,
+          commitment: 'processed' as Commitment,
+        },
+        serumProgramId,
+      );
+    }),
+  );
+
+  const quoteToken = new Token(
+    connection,
+    spotMarkets[0].quoteMintAddress,
+    TOKEN_PROGRAM_ID,
+    payer,
+  );
+  const quoteWallet = await quoteToken
+    .getOrCreateAssociatedAccountInfo(payer.publicKey)
+    .then((a) => a.address);
+
+  const baseWallets = await Promise.all(
+    spotMarkets.map((m) => {
+      const token = new Token(
+        connection,
+        m.baseMintAddress,
+        TOKEN_PROGRAM_ID,
+        payer,
+      );
+
+      return token
+        .getOrCreateAssociatedAccountInfo(payer.publicKey)
+        .then((a) => a.address);
+    }),
+  );
+
+  const eventQueuePks = spotMarkets.map(
+    (market) => market['_decoded'].eventQueue,
+  );
+
+  // eslint-disable-next-line
+  while (true) {
+    const eventQueueAccts = await getMultipleAccounts(
+      connection,
+      eventQueuePks,
+    );
+
+    for (let i = 0; i < eventQueueAccts.length; i++) {
+      const accountInfo = eventQueueAccts[i].accountInfo;
+      const events = decodeEventQueue(accountInfo.data);
+
+      if (events.length === 0) {
+        continue;
+      }
+
+      const accounts: Set<string> = new Set();
+      for (const event of events) {
+        accounts.add(event.openOrders.toBase58());
+
+        // Limit unique accounts to first 10
+        if (accounts.size >= maxUniqueAccounts) {
+          break;
+        }
+      }
+
+      const openOrdersAccounts = [...accounts]
+        .map((s) => new PublicKey(s))
+        .sort((a, b) => a.toBuffer().swap64().compare(b.toBuffer().swap64()));
+
+      const instr = DexInstructions.consumeEvents({
+        market: spotMarkets[i].publicKey,
+        eventQueue: spotMarkets[i]['_decoded'].eventQueue,
+        coinFee: baseWallets[i],
+        pcFee: quoteWallet,
+        openOrdersAccounts,
+        limit: consumeEventsLimit,
+        programId: serumProgramId,
+      });
+
+      const transaction = new Transaction();
+      transaction.add(instr);
+
+      console.log(
+        'market',
+        i,
+        'sending consume events for',
+        events.length,
+        'events',
+      );
+      await connection.sendTransaction(transaction, [payer], {
+        skipPreflight: true,
+        maxRetries: 2,
+      });
+    }
+    await sleep(interval);
+  }
+}
+
+run();
